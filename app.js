024147cc3434cbe5f89d5d9ba88e35431c4ed27c
migueldfh <miguel.dlafuenteh@gmail.com>
2025-04-14 23:32:02 -0600

    listens to all interfaces

--- app.js
+++ app.js
@@ -263,6 +263,6 @@ app.get('/admin', authMiddleware, (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
